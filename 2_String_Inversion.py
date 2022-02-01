37418f6049054359244e703088ed38a71f255d01
anuragrao04 <raoanu2004@gmail.com>
2022-02-01 12:47:36 +0530

    added stack in program 2

--- 2_String_Inversion.py
+++ 2_String_Inversion.py
@@ -1,6 +1,6 @@
 # 2. Write a program to input a line of text from the input terminal into stack and output the
 # string in the reveres order, each character appearing twice. (Eg. the string a b c d e should
 # be ee dd cc bb aa)
-
-string = input("Enter String to invert: ")[::-1]
-for i in string: print(i*2, end = "")
+stack = []
+for i in input("Enter String to invert: ")[::-1]: stack.append(i)
+for i in stack: print(i*2, end = "")
